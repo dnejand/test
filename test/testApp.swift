2be2dcb418744b8192e4610b9d7ce5b17df7f466
dnejand <dnejand@gmail.com>
2021-10-08 19:41:13 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Brian Nejand on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
